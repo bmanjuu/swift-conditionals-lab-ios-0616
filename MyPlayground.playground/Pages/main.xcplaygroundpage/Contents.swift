82b9619c10da3404b74d1c23dd1249fb90ff6d36
bmanjuu <bettyofung@gmail.com>
2016-07-07 10:44:19 -0400

    almost done, up to the very last question yayayay

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,24 +25,28 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
+print(a >= b)
 
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
+print(a%b == 0)
 
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
+print(y*b <= a)
 
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
+print(!(a>=b))
 
 
 
@@ -50,12 +54,18 @@ let b = 32
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
+if a%b == 0 {
+    print("true")
+}
 
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
+if Double(a/b) > x {
+    print("true")
+}
 
 
 
@@ -63,6 +73,12 @@ let b = 32
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
+if(y/Int(x)) > 3 {
+    print("true")
+}
+else {
+    print("false")
+}
 
 
 
@@ -71,7 +87,9 @@ let b = 32
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
 // write your code here
-
+if Double(y)>x && (a/b)>9 {
+    print("true")
+}
 
 
 
@@ -80,6 +98,16 @@ let b = 32
 */
 // write your code here
 
+func isGreater (number1: Int, number2: Int) -> Bool {
+    
+    if number1 > number2 {
+        return true
+    }
+    else {
+        return false
+    }
+    
+}
 
 
 
@@ -90,12 +118,30 @@ let b = 32
 */
 // write your code here
 
+func isForceWith (name: String) -> Bool {
+    
+    let theForceIsStrongInThese = ["Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader"]
+    
+    if theForceIsStrongInThese.contains(name){
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
+
+isForceWith("Chewbacca")
+isForceWith("Leia")
+
 
 
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
+
+
 
 
 
